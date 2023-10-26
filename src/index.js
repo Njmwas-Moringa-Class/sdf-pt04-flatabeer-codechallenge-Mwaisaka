61f253ec3b3d388747760bb0bd92071d1e99ca53
Frank <frankkashere@gmail.com>
2023-10-26 18:54:29 +0300

    Updated assignment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 // Code here
 document.addEventListener('DOMContentLoaded',() => {
 
-    const baseUrl='http://localhost:3000/beers';
+    const baseUrl='http://localhost:3000';
 
     const beerTitle = document.getElementById('beer-name');
     const beerImage = document.getElementById('beer-image');
     const beerDescription = document.getElementById('beer-description');
     const reviewList = document.getElementById('review-list');
 
+    const listItem = document.createElement("li");
+
     //See the full details of the firt beer (i.e. its name, image, description, and reviews) when the page loads.
     //Fetch the data of the beer from the server.
    
-    fetch(baseUrl)
+    fetch(`${baseUrl}/beers`)
     .then(response =>response.json())
     .then((beers) => {
 
@@ -23,14 +25,14 @@ document.addEventListener('DOMContentLoaded',() => {
     });
 
     //The below code diplays the list of beers on the 'nav'
-    fetch(baseUrl)
+    fetch(`${baseUrl}/beers`)
     .then(res=>res.json())
     .then((data)=>{
         data.forEach(beer=>{
         
         const beerList = document.getElementById("beer-list");
         const listItem = document.createElement("li");
-                
+              
         listItem.textContent=beer.name;
         beerList.appendChild(listItem);
     });
